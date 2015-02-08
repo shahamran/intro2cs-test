bb3a530a038d0551f1ab2d5aff3d8410170159ba
Ran Shaham <shahamran91@gmail.com>
2015-02-08 11:54:23 +0200

    Add runtime complexity explaination for partial_sums.py

--- partial_sums.py
+++ partial_sums.py
@@ -12,3 +12,8 @@ def partial_sum(lst, num):
 		exists = partial_sum(new_lst, num - x)
 		if exists: return exists + [x]
 	return
+
+""" Worst case runtime complexity: O(2 ** n)
+    That's because in this case every sub-set of lst will be checked
+    There are 2 ** n such sub-sets. """
+
