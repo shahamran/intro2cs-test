9099dbd99563b3556a8816cf2f0d1c7c0a549e6b
Ran Shaham <shahamran91@gmail.com>
2015-02-09 13:45:46 +0200

    Add 2013 stuff

---
+++ questions_2013.py
@@ -0,0 +1,65 @@
+# ex 1
+def base_to_base(num, base1, base2):
+	cnt = 0
+	result = 0
+	while num > 0:
+		result += (num % base2) * (base1 ** cnt)
+		num //= base2
+		cnt += 1
+	return result
+	
+# ex 7
+def flatten(L):
+	result = []
+	for i in range(len(L)):
+		item = L[i]
+		if not type(item) is list:
+			result.append(item)
+		else:
+			result += flatten(item)
+	return result
+
+# WordCombinations
+class WordCombinations:
+	def __init__(self, word_list):
+		self.word_list = word_list
+		self.curr_idx = 0
+		self.all_words = 1
+		for lst in word_list:
+			lst.sort() # b
+			self.all_words *= len(lst)
+
+	def __iter__(self):
+		return self
+
+	def __next__(self):
+		if self.curr_idx >= self.all_words: raise StopIteration
+		N = len(self.word_list)
+		SEP = ' '
+		END = '.'
+		sentence = ''
+		for i in range(N):
+			lst = self.word_list[i]
+			others = 1
+			for j in range(N - 1, i, -1):
+				others *= len(self.word_list[j])
+			idx = self.curr_idx // (others) % len(lst)
+			sentence += self.word_list[i][idx]
+			sentence += SEP if i < N - 1 else END
+		self.curr_idx += 1
+		return sentence
+
+# print_reverse()
+def read():
+	return int(input())
+
+def print_reverse():
+
+	def helper(last = None):
+		x = read()
+		# print(x) (iv)
+		if not last or x >= last:
+			helper(x)
+		print(x) # <-- delete (iv)
+
+	helper()
---
+++ test_questions_2013.py
@@ -0,0 +1,44 @@
+from questions_2013 import *
+
+def test_wc():
+	word_list = [['I','You','We'],['love','hate'],['loving','hating','studying']]
+	wc = WordCombinations(word_list)
+	for sentence in wc:
+		print(sentence)
+def test_reverse():
+	print_reverse()
+
+def test_bases():
+	def legal(num, base1, base2):
+		numbers = num.isnumeric() and \
+				  base1.isnumeric() and \
+				  base2.isnumeric()
+		if not numbers: return
+		num = int(num)
+		base1 = int(base1)
+		base2 = int(base2)
+		base1_legal = base1 <= 10 and base1 >= 2
+		base2_legal = base2 <= 10 and base2 >= 2
+		if base1_legal and base2_legal:
+			return num,base1,base2
+
+	while True:
+		num = input("Enter a number to be converted: ")
+		base1 = input("From base: ")
+		base2 = input("To base: ")
+		is_legal = legal(num,base1,base2)
+		if not is_legal:
+			print('Illegal input. Try again.')
+		else:
+			num = is_legal[0]
+			base1 = is_legal[1]
+			base2 = is_legal[2]
+			break
+
+	result = base_to_base(num,base1,base2)
+	print(str(num) + " in base " + str(base1) + " is "\
+	    + str(result) + " in base " + str(base2))
+
+def test_flatten(lst):
+	print(flatten(lst))
+	
